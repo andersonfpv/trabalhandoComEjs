b99cce402ebe5c2e1eac94fc0cc130c86102e6fa
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-05-01 19:27:59 -0300

    finalizando com forEach para gerar um jogral no index

--- server.js
+++ server.js
@@ -4,7 +4,38 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-    res.render("pages/index");
+    const items = [
+        { 
+            title: "D",
+            message: "esenvolver aplicações/serviços de forma fácil"
+        },
+        { 
+            title: "E",
+            message: "JS usa JavaScript para renderizar HTML"
+        },
+        { 
+            title: "M",
+            message: "uito fácil de usar"
+        },
+        { 
+            title: "A",
+            message: "prender sempre é fascinante"
+        },
+        { 
+            title: "I",
+            message: "nspirador poder criar sua aplicação"
+        },
+        { 
+            title: "S",
+            message: "intax simples"
+        }
+    ];
+
+    const subtitle = "Uma linguagem de modelagem para criação de página HTML utilizando JavaScript"
+    res.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle,
+    });
 })
 
 app.get("/sobre", function(req, res) {
